38e09343b5c3239bf587360ce8a2d9ae6744d140
ChiawNa <chiawnak0390@gmail.com>
2025-03-20 19:05:59 +0800

    javascipt done

---
+++ P2 - Build a Roman Numeral Converter Project/script.js
@@ -0,0 +1,66 @@
+const number = document.getElementById('number');
+const convertBtn = document.getElementById('convert-btn');
+const output = document.getElementById('output');
+
+const checkUserInput = () => {
+
+    output.classList.remove("error");
+
+    if (number.value === ""){
+        output.innerHTML = "Please enter a valid number.";
+        output.classList.add("error");
+        output.classList.remove("hidden"); 
+    }
+    else if (number.value < 1){
+        output.innerHTML = "Please enter a number greater than or equal to 1.";
+        output.classList.add("error");
+        output.classList.remove("hidden"); 
+    }
+    else if (number.value > 3999){
+        output.innerHTML = "Please enter a number less than or equal to 3999.";
+        output.classList.add("error");
+        output.classList.remove("hidden"); 
+    }
+    else {
+        convertToRoman(number.value);
+    }
+}
+
+const convertToRoman = (num) => {
+    const romanNumerals = [
+        { value: 1000, numeral: 'M' },
+        { value: 900, numeral: 'CM' },
+        { value: 500, numeral: 'D' },
+        { value: 400, numeral: 'CD' },
+        { value: 100, numeral: 'C' },
+        { value: 90, numeral: 'XC' },
+        { value: 50, numeral: 'L' },
+        { value: 40, numeral: 'XL' },
+        { value: 10, numeral: 'X' },
+        { value: 9, numeral: 'IX' },
+        { value: 5, numeral: 'V' },
+        { value: 4, numeral: 'IV' },
+        { value: 1, numeral: 'I' }
+    ];
+
+    let result = "";
+    for(const i of romanNumerals){
+        while(num >= i.value){
+            result += i.numeral;
+            num -= i.value;
+        }
+    }
+
+    output.innerHTML = result;
+}
+
+convertBtn.addEventListener('click', () => {
+    output.classList.remove("hidden");
+    checkUserInput();
+})
+
+number.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      checkUserInput();
+    }
+  });
