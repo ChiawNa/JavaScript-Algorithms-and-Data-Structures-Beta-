33e9a918b38671333efaf4004f9a3a7b9dc8d899
ChiawNa <chiawnak0390@gmail.com>
2025-03-26 18:19:45 +0800

    fully done

---
+++ P3 - Build a Telephone Number Validator Project/script.js
@@ -0,0 +1,38 @@
+const userInput = document.getElementById("user-input");
+const checkButton = document.getElementById("check-btn");
+const clearButton = document.getElementById("clear-btn");
+const resultsDiv = document.getElementById("results-div");
+
+const validatePhoneNumber = (number) => {
+    const regex = /^(1\s?)?(\(\d{3}\)|\d{3})[-\s]?\d{3}[-\s]?\d{4}$/;
+    return regex.test(number);
+};
+
+const checkUserInput = () => {
+    
+    if (userInput.value === "") {
+        alert("Please provide a phone number");
+        return;
+    }
+
+    const isValid = validatePhoneNumber(userInput.value);
+    resultsDiv.innerHTML = `
+    <p class="${isValid ? 'valid' : 'invalid'}">
+        ${isValid ? 'Valid' : 'Invalid'} US number: ${userInput.value}
+    </p>
+    `;
+};
+
+const clearInput = () => {
+    userInput.value = "";
+    resultsDiv.innerHTML = "";
+};
+
+checkButton.addEventListener("click", checkUserInput);
+clearButton.addEventListener("click", clearInput);
+
+userInput.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        checkUserInput();
+    }
+});
